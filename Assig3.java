d4d97e5aeab6a5272b15853a147a416520444ac7
= <=>
2015-10-09 20:18:21 -0400

    meh

--- Assig3.java
+++ Assig3.java
@@ -3,15 +3,12 @@ import java.util.Random;
 public class Assig3 {
 
     public static void main(String[] main){
-
-        while (true) {
-    
+        
         Random rd = new Random();
         Dice dice1 = new Dice(rd);
         int theroll = dice1.roll();
     
         System.out.println("The roll is " + theroll);
         
-        }
     }
 }
