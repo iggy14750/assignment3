9c2e87ef43fd1ab9fb243a3ff013eb1e8d70714d
Isaac <isaacgoss14750@gmail.com>
2015-10-12 14:56:17 -0400

    I got something to work!

--- Assig3.java
+++ Assig3.java
@@ -3,17 +3,21 @@ public class Assig3 {
 
     public static void main(String[] main){
         
-        Dice dice1 = new Dice();
+        /* Dice dice1 = new Dice();
         int theroll = dice1.roll();
     
-        System.out.println("The roll is " + theroll);
+        System.out.println("The roll is " + theroll); */
+        
+        Player jimbo = new Player("Jimbo", "Smithy", 44.1, 13, 1);
+        
+        jimbo.printInfo();
         
     }
     
-    private static String[] randomReply(Dice dc){
+    /* private static String[] randomReply(Dice dc){
         Srtring[] replies = new String[6];
         replies = {"Don't worry; everyone gets a little shy at these things.", "Don't have a seizure on me!", "You kiss your mother with that mouth?", "MY LEG!"};
         which = dc.roll()-1;
         return replies[which];
-    }
+    } */
 }
--- Player.java
+++ Player.java
@@ -6,16 +6,16 @@ public class Player {
     /*How can I present a double as a string 
     with two decimal points?, like %.2f in printf.
     */
-    private int _rounds=0;
     private int _wins=0;
-    private int _totalRounds;
+    private int _totalRounds=0;
     
-    public Player(String lname, String fname, double cash, int totalRounds){
+    public Player(String lname, String fname, double cash, int totalRounds, int wins){
         
         _lname = lname;
         _fname = fname;
         _cash = cash;
         _totalRounds = totalRounds;
+        _wins = wins;
     }
     
     public double cashLeft(){
@@ -23,14 +23,14 @@ public class Player {
     }
     
     public void wonAGame(double winnings){
-        _rounds++;
+        _totalRounds++;
         _wins++;
         _cash+=winnings;
     }
     
-    public void lostAGame(){
+    public void lostAGame(double bet){
         _rounds++;
-        _cash-=bet;//this needs to be determined later
+        _cash-=bet;
     }
     
     public void addMoney(double clams){//this may not be necssary...
@@ -39,9 +39,9 @@ public class Player {
         }
     }
     
-    public String[] niceString(){ //looking at this further in testing
-        String[] info = new String(_fname + " " + _lname + " has $" + _cash + ", and is " + _wins + "/" + _rounds);
-        return info;
+    public void printInfo(){
+        System.out.println("Here is your current information:");
+        System.out.printf("\tName: %s %s\n\tMoney Left: $%.2f\n\tTotal Rounds Played: %d\n\tTotal Rounds Won: %d\n\n", _fname, _lname, _cash, _totalRounds, _wins);
     }
 }
 
